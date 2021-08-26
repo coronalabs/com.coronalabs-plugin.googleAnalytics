627cc08a59645c551ed40f035fe599e8113046a9
Scott Harrison <scottrules44@gmail.com>
2021-08-26 13:34:56 -0500

    add source code
    add source code

---
+++ src/ios/Plugin/googleAnalytics.h
@@ -0,0 +1,16 @@
+//
+//  googleAnalytics.h
+//  Google Analytics Plugin
+//
+//  Copyright (c) 2021 Solar2d All rights reserved.
+//
+
+#ifndef _GoogleAnalytics_H__
+#define _GoogleAnalytics_H__
+
+#include <CoronaLua.h>
+#include <CoronaMacros.h>
+
+CORONA_EXPORT int luaopen_plugin_googleAnalytics( lua_State *L );
+
+#endif
